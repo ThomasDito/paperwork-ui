2e59b0b627186ccd3e62670e6d01ce28d11bc556
natainditama <natainditama.dev@gmail.com>
2023-09-18 13:41:59 +0800

    Fix invalid input style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,8 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          // "border-input bg-card ring-offset-background placeholder:text-muted-foreground focus-visible:ring-primary/0 focus:border-primary invalid:border-danger invalid:ring-danger/50 flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm file:border-0 file:bg-transparent file:text-sm file:font-medium invalid:ring-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 invalid:text-danger",
-          "border-input bg-card ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring invalid:text-danger invalid:ring-danger focus-visible:invalid:ring-danger flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm file:border-0 file:bg-transparent file:text-sm file:font-medium invalid:ring-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input bg-card ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
