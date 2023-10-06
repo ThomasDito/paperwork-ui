0d9a624e9ec8aba6bcf027eab366e38e89cab245
natainditama <natainditama.dev@gmail.com>
2023-10-06 19:37:41 +0800

    Resolve Tailwind config issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+// this file just for Tailwind CSS IntelliSense, so don't need add any config
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
